c1a68f2475c00b738aa6120bab76902f0331df15
intrapus <60838036+intrapus@users.noreply.github.com>
2023-08-26 21:07:59 +0300

    add debugging port

--- youtubeviewer/basics.py
+++ youtubeviewer/basics.py
@@ -115,6 +115,7 @@ def get_driver(background, viewports, agent, auth_required, path, proxy, proxy_t
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-features=UserAgentClientHint')
     options.add_argument("--disable-web-security")
+    options.add_argument("--remote-debugging-port=9222")
     webdriver.DesiredCapabilities.CHROME['loggingPrefs'] = {
         'driver': 'OFF', 'server': 'OFF', 'browser': 'OFF'}
 
